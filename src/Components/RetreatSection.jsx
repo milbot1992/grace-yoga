837fd6cb420596119b5f5d6df0472564d4ac96d5
Millie Ellis <millieellis@Millies-MacBook-Pro.local>
2024-04-16 14:55:36 +0100

    updated spacing

--- src/Components/RetreatSection.jsx
+++ src/Components/RetreatSection.jsx
@@ -11,7 +11,7 @@ export default function RetreatSection() {
                 <div className='retreat-text'>
                     <h1>Reset Retreat 2024</h1>
                     <p>Join me for a Reset Yoga Retreat in beautiful Archidona, Spain. This Yoga Retreat offers a blissful and relaxing 4-night getaway, where you'll immerse yourself in rejuvenating practices amidst the serene landscapes of southern Spain.</p>
-                    <p>Thursday 19th - Monday 23rd September 4 Nights, 4 Days.</p>
+                    <p>Thursday 19th - Monday 23rd September</p>
                     <p className='button'>
                         <Link to="/retreat" className="find-out-more-button">
                             Click here to find out more
